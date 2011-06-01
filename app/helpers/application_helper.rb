e96af387f891f71121dd430d5432f0c8fb2bd892
Taylor <tbrowning@me.com>
2011-06-01 15:35:11 -0400

    Personalized the site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   #Returns a title on a per-page basis.
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "TaylorTwitter"
     if @title.nil?
       base_title
     else
